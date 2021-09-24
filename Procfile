d8ba7cbb89982154ef497f8b743faa09b374d467
Zizou897 <azeridwan10@gmail.com>
2021-09-24 10:29:07 +0000

    test

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn nan.wsgi
--- nan/settings.py
+++ nan/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -148,3 +149,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
