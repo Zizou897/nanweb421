fdec68d7ccb284d0e7360f1826123fc7aee47405
Zizou897 <azeridwan10@gmail.com>
2021-09-24 14:18:06 +0000

    setting

--- nannews/models.py
+++ nannews/models.py
@@ -8,6 +8,7 @@ class Contact(models.Model):
     nom = models.CharField(max_length=250)
     email = models.EmailField()
     message = models.TextField()
+
     date_add = models.DateTimeField(auto_now_add=True)
     date_update = models.DateTimeField(auto_now=True)
     status = models.BooleanField(default=True)
@@ -22,6 +23,7 @@ class Contact(models.Model):
 
 class Newsletter(models.Model):
     email = models.EmailField()
+    
     date_add = models.DateTimeField(auto_now_add=True)
     date_update = models.DateTimeField(auto_now=True)
     status = models.BooleanField(default=True)
