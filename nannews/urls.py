8e20612871912b4a0a000f12653d7d75f00d8c2a
Zizou897 <azeridwan10@gmail.com>
2021-10-07 15:59:41 +0000

    a refaire

--- nannews/urls.py
+++ nannews/urls.py
@@ -4,10 +4,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("catagory", views.catagory, name="catagory"),
-    path("contact", views.contact, name="contact"),
-    path("about", views.about, name="about"),
-    path("detail", views.detail, name="detail"),
-    path('checkup', views.checkup, name='checkup'),
+    path("catagory/", views.catagory, name="catagory"),
+    path("contact/", views.contact, name="contact"),
+    path("about/", views.about, name="about"),
+    path("detail/", views.detail, name="detail"),
+    path('checkup/', views.checkup, name='checkup'),
     path('registerEmail/', views.registerEmail , name='registerEmail')
 ]
