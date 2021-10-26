9fb0daf174970a3c77e89e707d2e9abbd1029df9
Zizou897 <azeridwan10@gmail.com>
2021-10-26 15:21:52 +0000

    is ready

--- nannews/urls.py
+++ nannews/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("contact/", views.contact, name="contact"),
     path("about/", views.about, name="about"),
     path("detail/", views.detail, name="detail"),
-    path("detail/<int:idt>/", views.detail),
+    path("detail/<str:idt>/", views.detail),
     path('checkup/', views.checkup, name='checkup'),
     path('registerEmail/', views.registerEmail , name='registerEmail')
 ]
